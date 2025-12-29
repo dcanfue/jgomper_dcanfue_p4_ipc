58f63c2cae8aadce1b855ba1351d5172a924365f
jgomper <jgomper@upv.edu.es>
2025-12-29 15:53:51 +0100

    Añadir vista para el listado de productos

--- lib/views/list_view.dart
+++ lib/views/list_view.dart
@@ -1,4 +1,4 @@
-import 'dart:convert';
+import 'dart:convert'; 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:cached_network_image/cached_network_image.dart';
